67a489af1fcaa65e35a69a4841f3ed00615dc939
matteo-giustini <matteo.giustini3@gmail.com>
2024-12-03 17:48:04 +0100

    init. project

---
+++ dadi/script.js
@@ -0,0 +1,18 @@
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+
+/*-1 STABILISCO I NUMERI DA 1 A 6
+  -2 CHIEDO ALL'UTENTE SE VUOLE GIOCARE
+  - SE SI VADO AVANTI
+  - SE NO CHIEDO DI NUOVO
+  -3 ESTRAGGO NUMERO-UTENTE
+  -4 NOTIFICO NUMERO-UTENTE
+  -5 ESTRAGGO NUMERO-CPU 
+  -6 NOTIFICO NUMERO-CPU
+  -COMPARO I DUE NUMERI
+   SE NUTENTE > MAGGIORE NCPU AVVISO
+   SE NUTENTE < MINORE NCPU AVVISO
+   SE NIENTE NUTENTE = NCPU AVVISO
---
+++ email/script.js
@@ -0,0 +1,11 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email.*/
+
+//1-DICHIARO LISTA EMAIL
+//2-CREO CONST PER LA CONFERMA
+//3-CHIEDO DI INSERIRE UNA EMAIL
+//4 SE L'EMAIL COINCIDE CON UNA DELLA LISTA EMAIL CAMBIO IL PARAMETRO CONFERMA E INVIO MESSAGIO DELL'ESITO POSITIVO
+//  SE NO, INVIO MESSAGGIO DELL'ESITO NEGATIVO E RITORNO AL PUNTO 3
+    
